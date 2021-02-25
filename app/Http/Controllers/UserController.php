ce5d3b06d4102edf9704c75d358b5f71f0bc393b
NafisaIsrat <nafisawhy009@gmail.com>
2021-02-25 23:57:31 +0600

    Home page updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,10 +9,14 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     //
+    /**
+     * @param Request $req
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|string
+     */
     function login (Request $req)
     {
-        $user = User::where(['email'=>$req->email])->firstorFail();
-        if($user || Hash::check($req->password, $user->password))
+        $user = User::where('email',request('email'))->first();
+        if($user || Hash::check(request('password'), $user->password))
         {
             return "Username or Password isnot match.";
         }
